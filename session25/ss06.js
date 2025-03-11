92189affd7125f1c0b3a6642ece5f6d13af0ae47
chunghoang123 <chungss7890@gmail.com>
2025-03-11 20:46:33 +0700

    first commit

--- session25/ss06.js
+++ session25/ss06.js
@@ -1,9 +1,21 @@
-let str = prompt("mời bạn nhập số để kiểm tra đối xứng");
-for (let i = str.length; i > 0; i--) {
-    if (str[i] == str) {
-        alert("chuỗi của bạn là đối xứng");
+let str= prompt(`Moi nhap chuoi:`)
+isPalindrome(str)
+function isPalindrome(str){
+    let arr = []
+    arr = str.split("")
+    let check = true;
+    let arr2 = [];
+    arr2 = arr.reverse();
+    let k = arr.length - 1;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] != arr2[k]) {
+        check = false;
+      }
+      k--;
     }
-    else {
-        alert("chuỗi của bạn không phải là đối xứng");
+    if (check) {
+      alert(`Chuoi doi xung`);
+    } else {
+      alert(`Chuoi khong doi xung`);
     }
-} 
+}
